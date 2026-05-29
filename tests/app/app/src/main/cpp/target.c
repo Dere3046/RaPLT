5673b38486953237047d45c6b3e2955315c69e41
dere3046 <fine4trn@163.com>
2026-05-29 19:16:33 +0800

    renametest

--- tests/app/app/src/main/cpp/target.c
+++ tests/app/app/src/main/cpp/target.c
@@ -1,15 +1,11 @@
-#include <stdlib.h>
-#include <string.h>
-#include <android/log.h>
-
-static int g_malloc_calls = 0;
-static int g_strlen_calls = 0;
+__attribute__((visibility("default")))
+int target_get_number(void)
+{
+    return 42;
+}
 
 __attribute__((visibility("default")))
-void trigger(void)
+int target_add(int a, int b)
 {
-    void *p = malloc(64);
-    free(p);
-    size_t n = strlen("raplt");
-    (void)n;
+    return a + b;
 }
